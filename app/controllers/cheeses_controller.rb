5d0c555e3350e658509cb55459f94978c6b20ec3
Chandelier Ephrata <chandephrata@gmail.com>
2022-05-16 20:18:48 -0600

    commit tree 7e2de6b9c32cb912fd183f7a1f30b56cfc167ac8

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -1,8 +1,15 @@
 class CheesesController < ApplicationController
 
+#GET /cheeses
   def index
     cheeses = Cheese.all
     render json: cheeses
   end
 
+  def show
+    cheese = Cheese.find(params[:id])
+    render json: cheese
+    # byebug
+  end
+  
 end
